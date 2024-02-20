9f90f972cda462b53f37d5a6c4b35d1dbb400efd
Konstantin Atroschenko <kostay375298918971@gmail.com>
2024-02-20 14:50:13 +0300

    feat: rule public-api добавил правила для testing public api

--- tests/lib/rules/public-api.js
+++ tests/lib/rules/public-api.js
@@ -38,6 +38,19 @@ ruleTester.run("public-api", rule, {
       errors: [],
       options: aliasOptions,
     },
+    {
+      filename: "E:\\git\\online-store-frontend\\src\\entities\\file.test.ts",
+      code: "import { BrandActions } from '@/entities/Brand/testing'",
+      errors: [],
+      options: testingOptions,
+    },
+    {
+      filename:
+        "E:\\git\\online-store-frontend\\src\\entities\\StoreDecorator.tsx",
+      code: "import { BrandActions } from '@/entities/Brand/testing'",
+      errors: [],
+      options: testingOptions,
+    },
   ],
 
   invalid: [
@@ -45,6 +58,21 @@ ruleTester.run("public-api", rule, {
       code: "import { BrandActions } from '@/entities/Brand/model/slices/BrandSlice'",
       errors: [{ message: "Абсолютный import разрешен только из index.ts" }],
       options: aliasOptions,
+    },
+    {
+      filename:
+        "E:\\git\\online-store-frontend\\src\\entities\\StoreDecorator.tsx",
+      code: "import { BrandActions } from '@/entities/Brand/example.tsx'",
+      errors: [{ message: "Абсолютный import разрешен только из index.ts" }],
+      options: testingOptions,
+    },
+    {
+      filename: "E:\\git\\online-store-frontend\\src\\entities\\example.tsx",
+      code: "import { BrandActions } from '@/entities/Brand/testing'",
+      errors: [
+        { message: "Тестовые данные необходимо импортировать из testing.ts" },
+      ],
+      options: testingOptions,
     },
   ],
 });
